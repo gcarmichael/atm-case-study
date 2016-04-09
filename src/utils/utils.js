27ece8a9bc198349caa643f94d31475984129d52
Gary Carmichael <g.carmichael90@gmail.com>
2016-04-09 18:40:28 +0100

    Implemented calculateCountNotes, tests passing. Todo: tidy.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -11,7 +11,7 @@ const Utils = {
    * @param  {Number} multiple - number which divides for
    * @return {Boolean}          -  true if is equals 0
    */
-  isMultipleOf( withdraw, multiple ) {
+   isMultipleOf( withdraw, multiple ) {
     return withdraw % multiple === 0;
   },
 
@@ -23,7 +23,7 @@ const Utils = {
    * @param  {Number} maxWithdraw - maximum number within the range
    * @return {Boolean}
    */
-  isValueWithinRange( withdraw, minWithdraw, maxWithdraw ) {
+   isValueWithinRange( withdraw, minWithdraw, maxWithdraw ) {
     return _.inRange( withdraw, minWithdraw, ( maxWithdraw + 1 ));
   },
 
@@ -35,7 +35,7 @@ const Utils = {
    * @return {Boolean} - Returns true if the initial amount is bigger
    * or equal than the amount compared
    */
-  isAnyMoneyLeft( totalMoney, withdraw ) {
+   isAnyMoneyLeft( totalMoney, withdraw ) {
     return totalMoney >= withdraw;
   },
 
@@ -58,15 +58,15 @@ const Utils = {
    * so an error can be returned for the availability validation.
    *
    */
-  areAnyNotesLeft( withdraw, notesContainer ) {
-  },
+   areAnyNotesLeft( withdraw, notesContainer ) {
+   },
 
   /** TODO Implement this method
    *
    * calculateCountNotes - Calculates the total count for each type of notes required for withdrawal.
    *
    * @param  {Number} withdraw   - Amount to calculate
-   * @param  {Object} notesContainer - ATM notes container
+   * @param  {Object} notesContainer - ATM notes container (Originally used props)
    * @return {Object} - Object of objects containing the count for each note
    *
    * The failing tests from "./test-utils/utilsSpec.js" can give you an idea about
@@ -76,7 +76,74 @@ const Utils = {
    * "./src/components/atm.js" is the parent component for this React app,
    * there you can understand more how this app works.
    */
-  calculateCountNotes( withdraw, props ) {
+   calculateCountNotes( withdraw, notesContainer ) {
+    var notes50Count = 0;
+    var notes20Count = 0;
+    var notes10Count = 0;
+    var withdraw = withdraw;
+
+    while (withdraw >= 50){
+      if(notesContainer.atmData.notesContainer['50'].count != 0){
+        var withdraw = withdraw - 50;
+        notes50Count += 1;
+      }
+      if (withdraw >= 20){
+        if(notesContainer.atmData.notesContainer['20'].count != 0){
+          var withdraw = withdraw - 20;
+          notes20Count += 1;
+        }
+      }
+      if (withdraw >= 10){
+        if(notesContainer.atmData.notesContainer['10'].count != 0){
+          var withdraw = withdraw - 10;
+          notes10Count += 1;
+        }
+      }
+    }
+
+    while (withdraw >= 20){
+      if(notesContainer.atmData.notesContainer['20'].count != 0){
+        var withdraw = withdraw - 20;
+        notes20Count += 1;
+      }
+    }
+
+    while (withdraw > 0){
+      if(notesContainer.atmData.notesContainer['10'].count != 0){
+        var withdraw = withdraw - 10;
+        notes10Count += 1;
+      }
+    }
+
+    // while (withdraw >= 0) {
+    //   var notes50Needed = Math.floor(withdraw / 50);
+    //   if (notes50Needed <= notesContainer && notes50Needed > 1) {
+    //     withdraw -= 50;
+    //     var notes50Count = notes50Count + 1;
+    //     var notes20Needed = Math.floor(withdraw / 20);
+    //     if (notes20Needed <= notesContainer && notes20Needed > 1) {
+    //       withdraw -= 20;
+    //       var notes20Count = notes20Count + 1;
+    //       var notes10Needed = Math.floor(withdraw / 10);
+    //       if (notes10Needed <= notesContainer && notes10Needed > 1) {
+    //         withdraw -= 10;
+    //         var notes10Count = notes10Count + 1;
+    //       }
+    //     }
+    //   }
+    // };
+
+    return {
+      50: {
+        count: notes50Count
+      },
+      20: {
+        count: notes20Count
+      },
+      10: {
+        count: notes10Count
+      }
+    };
   },
 
   /**
@@ -88,7 +155,7 @@ const Utils = {
    * @param  {Object} availableNotes - Object of objects with the count of each note
    * @return {Object} - Object containing the new amount of each note
    */
-  subtractCountFromTotal( withdrawnNotes, availableNotes ) {
+   subtractCountFromTotal( withdrawnNotes, availableNotes ) {
     const notes50Used = _.get( withdrawnNotes, '50' );
     const notes20Used = _.get( withdrawnNotes, '20' );
     const notes10Used = _.get( withdrawnNotes, '10' );
@@ -118,7 +185,7 @@ const Utils = {
    *
    * @return {String} - Message that depends on the object passed
    */
-  getValidationMessage( validationObject ) {
+   getValidationMessage( validationObject ) {
     const validationMsg = {
       notesError: 'There is only notes of £10, £20 and £50',
       notesAvailability: 'The only available notes at this time are  ',
@@ -138,7 +205,7 @@ const Utils = {
    * @param  {Object} props - Global React props "location.pathname"
    * @return {String} - Message that depends on the url passed
    */
-  getScreenMessage( props ) {
+   getScreenMessage( props ) {
     const screenMsg = {
       '/': `Hello, ${props.atmData.user.name}, welcome to iDotBank`,
       '/withdraw': `${props.atmData.user.name}, do your withdraw`,
@@ -154,7 +221,7 @@ const Utils = {
    * @param  {Object} props - Global React props
    * @return {String} - User's account balance
    */
-  getBalanceMessage( props ) {
+   getBalanceMessage( props ) {
     const { user } = props.atmData;
     return `${user.name}, your current balance account is £${user.accountBalance}`;
   },
@@ -165,7 +232,7 @@ const Utils = {
    * @param  {Object} props - Global React props
    * @return {String} - Success message
    */
-  getSuccessMessage( props ) {
+   getSuccessMessage( props ) {
     const { name } = props.atmData.user;
     return `${name}, don't forget to take your money!`;
   },
@@ -181,7 +248,7 @@ const Utils = {
    * @param  {Object} withdrawnNotes - Total count of notes needed
    * @return {String} - Notes values
    */
-  displayWithdrawnCount( withdrawnNotes ) {
+   displayWithdrawnCount( withdrawnNotes ) {
     if ( withdrawnNotes[ '10' ] === undefined ) {
       return;
     }
